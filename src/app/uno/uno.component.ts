b88b78a285613c5fcee8c33a760251bd734157ed
Kevin <kevinarias099@hotmail.com>
2025-04-20 13:49:07 -0300

    ajuste baraja uno

--- src/app/uno/uno.component.ts
+++ src/app/uno/uno.component.ts
@@ -20,23 +20,27 @@ cartas = {
     color: ["rojo", "verde", "azul", "amarillo"],
     cartas: {
       numeros: [
-        { valor: "0", tipo: "numero" },
-        { valor: "1", tipo: "numero" },
-        { valor: "2", tipo: "numero" }, 
-        { valor: "3", tipo: "numero" }, 
-        { valor: "4", tipo: "numero" }, 
-        { valor: "5", tipo: "numero" }, 
-        { valor: "6", tipo: "numero" }, 
-        { valor: "7", tipo: "numero" },
-        { valor: "8", tipo: "numero" }, 
-        { valor: "9", tipo: "numero" }, 
+        { valor: 0, tipo: "numero" },
+        { valor: 1, tipo: "numero" },
+        { valor: 2, tipo: "numero" }, 
+        { valor: 3, tipo: "numero" }, 
+        { valor: 4, tipo: "numero" }, 
+        { valor: 5, tipo: "numero" }, 
+        { valor: 6, tipo: "numero" }, 
+        { valor: 7, tipo: "numero" },
+        { valor: 8, tipo: "numero" }, 
+        { valor: 9, tipo: "numero" }, 
       ],
       acciones: [
         { valor: "+2", tipo: "accion" }, 
         { valor: "salta", tipo: "accion" },
         { valor: "reversa", tipo: "accion" },
       ]
-    }
+    },
+    cartasEspeciales: [
+        { valor: "+4", tipo: "accion", colores: ["rojo", "verde", "azul", "amarillo"] }, 
+        { valor: "cambioColor", tipo: "accion", colores: ["rojo", "verde", "azul", "amarillo"] },
+    ]
   };
 constructor(){
 
