d6b99a678305b540792480c62e07671b6e1b7b72
Rodrigo <solisrodrigo619@gmail.com>
2025-04-21 12:35:17 -0300

    amiko

--- src/app/uno/uno.component.ts
+++ src/app/uno/uno.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component, ChangeDetectorRef  } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 interface Jugador {
@@ -8,169 +8,76 @@ interface Jugador {
   puntaje: number;
   cartas: Array<any>;
 }
+
 @Component({
   selector: 'app-uno',
-  imports: [RouterOutlet,RouterLink, RouterLinkActive, NgFor, CommonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgFor, CommonModule],
   templateUrl: './uno.component.html',
-  styleUrl: './uno.component.css'
+  styleUrl: './uno.component.css',
 })
 export class UnoComponent {
-
   nuevoJugador: any;
   barajaActiva: Array<any> = [];
   bComenzoJuego: boolean = false;
-  
-carta = {
-    color: [{nombre:"rojo", codColor: '#EB5353'},{nombre:"verde", codColor: '#36AE7C'},{nombre:"azul", codColor: '#187498'},{nombre:"amarillo", codColor: '#F9D923'}],
+
+  carta = {
+    color: [
+      { nombre: 'rojo', codColor: '#EB5353' },
+      { nombre: 'verde', codColor: '#36AE7C' },
+      { nombre: 'azul', codColor: '#187498' },
+      { nombre: 'amarillo', codColor: '#F9D923' },
+    ],
     cartas: [
-      { valor: 0, tipo: "numero" },
-      { valor: 1, tipo: "numero" },
-      { valor: 2, tipo: "numero" }, 
-      { valor: 3, tipo: "numero" }, 
-      { valor: 4, tipo: "numero" }, 
-      { valor: 5, tipo: "numero" }, 
-      { valor: 6, tipo: "numero" }, 
-      { valor: 7, tipo: "numero" },
-      { valor: 8, tipo: "numero" }, 
-      { valor: 9, tipo: "numero" },
-      { valor: "+2", tipo: "accion" }, 
-      { valor: "salta", tipo: "accion" },
-      { valor: "reversa", tipo: "accion" },
-      { valor: "+4", tipo: "accion"}, 
-      { valor: "cambioColor", tipo: "accion"},
-    ]
-  };
-constructor(private cdr: ChangeDetectorRef){
-  this.mezclarCartasYRepartir();
-}
-
-
-private obtenerCartaAleatoria(): any {
-  const indiceAleatorioCartas = Math.floor(Math.random() * this.carta.cartas.length);
-  const indiceAleatorioColor = Math.floor(Math.random() * this.carta.color.length);
-
-  const cartaSeleccionada = {
-    ...this.carta.cartas[indiceAleatorioCartas],
-    color: (indiceAleatorioCartas === 13 || indiceAleatorioCartas === 14)
-      ? { nombre: "especial", codColor: "#19282F" }
-      : this.carta.color[indiceAleatorioColor]
+      { valor: 0, tipo: 'numero' },
+      { valor: 1, tipo: 'numero' },
+      { valor: 2, tipo: 'numero' },
+      { valor: 3, tipo: 'numero' },
+      { valor: 4, tipo: 'numero' },
+      { valor: 5, tipo: 'numero' },
+      { valor: 6, tipo: 'numero' },
+      { valor: 7, tipo: 'numero' },
+      { valor: 8, tipo: 'numero' },
+      { valor: 9, tipo: 'numero' },
+      { valor: '+2', tipo: 'accion' },
+      { valor: 'salta', tipo: 'accion' },
+      { valor: 'reversa', tipo: 'accion' },
+      { valor: '+4', tipo: 'accion' },
+      { valor: 'cambioColor', tipo: 'accion' },
+    ],
   };
 
-  return cartaSeleccionada;
-}
+  constructor(private cdr: ChangeDetectorRef) {
+    this.mezclarCartasYRepartir();
+  }
 
-mezclarCartasYRepartir() {
-  this.nuevoJugador = {
-    id: 1,
-    nombre: "Momo",
-    puntaje: 0,
-    cartas: []
-  };
-
-  console.log(this.nuevoJugador);
-
-  while (this.nuevoJugador.cartas.length < 7) {
+  private obtenerCartaAleatoria(): any {
     const indiceAleatorioCartas = Math.floor(Math.random() * this.carta.cartas.length);
     const indiceAleatorioColor = Math.floor(Math.random() * this.carta.color.length);
-    console.log(indiceAleatorioCartas)
 
-
-    if(indiceAleatorioCartas == 13 || indiceAleatorioCartas == 14){
-      this.nuevoJugador.cartas.push({
-        ...this.carta.cartas[indiceAleatorioCartas],
-        color: {nombre:"especial", codColor: '#19282F'}
-      });
-  
-    }
-    else{
-      this.nuevoJugador.cartas.push({
-        ...this.carta.cartas[indiceAleatorioCartas],
-        color: this.carta.color[indiceAleatorioColor]
-      });
-  
-    }
-
-    console.log("cartas", this.nuevoJugador.cartas)
+    const esEspecial = indiceAleatorioCartas === 13 || indiceAleatorioCartas === 14;
+    return {
+      ...this.carta.cartas[indiceAleatorioCartas],
+      color: esEspecial ? { nombre: 'especial', codColor: '#19282F' } : this.carta.color[indiceAleatorioColor],
+    };
   }
 
-    
-  const indiceAleatorioCartasTest = Math.floor(Math.random() * this.carta.cartas.length);
-  const indiceAleatorioColorTest = Math.floor(Math.random() * this.carta.color.length);
+  mezclarCartasYRepartir() {
+    this.nuevoJugador = { id: 1, nombre: 'Momo', puntaje: 0, cartas: [] };
 
-  if(indiceAleatorioCartasTest == 13 || indiceAleatorioCartasTest == 14){
-    this.barajaActiva.push({
-      ...this.carta.cartas[indiceAleatorioCartasTest],
-      color: {nombre:"especial", codColor: '#19282F'}
-    });
-
-  }
-  else{
-    this.barajaActiva.push({
-      ...this.carta.cartas[indiceAleatorioCartasTest],
-      color: this.carta.color[indiceAleatorioColorTest]
-    });
-
-  }
-
-this.bComenzoJuego = true;
-  //this.cdr.detectChanges();
-}
-
-agarrarCarta(){
-  const indiceAleatorioCartas = Math.floor(Math.random() * this.carta.cartas.length);
-    const indiceAleatorioColor = Math.floor(Math.random() * this.carta.color.length);
-    console.log(indiceAleatorioCartas)
-
-
-    if(indiceAleatorioCartas == 13 || indiceAleatorioCartas == 14){
-      this.nuevoJugador.cartas.push({
-        ...this.carta.cartas[indiceAleatorioCartas],
-        color: {nombre:"especial", codColor: '#19282F'}
-      });
-
-      this.barajaActiva.push({
-        ...this.carta.cartas[indiceAleatorioCartas],
-        color: {nombre:"especial", codColor: '#19282F'}
-      })
-  
-    }
-    else{
-      this.nuevoJugador.cartas.push({
-        ...this.carta.cartas[indiceAleatorioCartas],
-        color: this.carta.color[indiceAleatorioColor]
-      });
-
-      this.barajaActiva.push({
-        ...this.carta.cartas[indiceAleatorioCartas],
-        color: this.carta.color[indiceAleatorioColor]
-      })
-  
+    while (this.nuevoJugador.cartas.length < 7) {
+      this.nuevoJugador.cartas.push(this.obtenerCartaAleatoria());
     }
 
-    console.log("cartas", this.nuevoJugador.cartas)
-    console.log("cartasV", this.barajaActiva)
-}
+    this.barajaActiva.push(this.obtenerCartaAleatoria());
+    this.bComenzoJuego = true;
+  }
 
+  agarrarCarta() {
+    const nuevaCarta = this.obtenerCartaAleatoria();
+    this.nuevoJugador.cartas.push(nuevaCarta);
+    this.barajaActiva.push(nuevaCarta);
 
-jugarCarta(){
-
-}
-
-saltarJugador(){
-
-}
-
-cambiarSentido(){
-
-}
-
-darCarta(cantidad:any){
-
-}
-
-crupierGeneral(){
-
-}
-  
-
+    console.log('cartas', this.nuevoJugador.cartas);
+    console.log('cartasV', this.barajaActiva);
+  }
 }
